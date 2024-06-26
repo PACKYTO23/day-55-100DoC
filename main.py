393ccbb8e08be3b701d069984302415d7aa68bdd
Francisco_GP <91037607+PACKYTO23@users.noreply.github.com>
2024-06-26 09:26:38 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+def make_bold(function):
+    def style_function():
+        text = function()
+        print(f"{text}")
+    return style_function()
+
+
+@app.route("/")
+def hello_world():
+    return ("<h1 style='text-align: center'>Hello, World!</h1>"
+            "<p>This is a paragraph.</p>"
+            "<img src='https://thumbs.dreamstime.com/b/cartoon-alarm-clock-28115617.jpg' width=600>")
+
+
+@app.route("/bye")
+@make_bold
+# @make_emphasis
+# @make_underlined
+def bye():
+    return "Bye!"
+
+
+@app.route("/username/<name>/<int:number>")
+def greet(name, number):
+    return f"Hello there, {name}! Soon you'll have {number} million in the bank!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
